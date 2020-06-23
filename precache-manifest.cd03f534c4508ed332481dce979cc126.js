0584e0e1ac9e1351bba6d6b0fd1d494544802e0f
swamykankipati <kaswami507@gmail.com>
2020-06-23 09:19:51 +0530

    Updates

---
+++ precache-manifest.cd03f534c4508ed332481dce979cc126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c3ea4181c2285ee8e391d8d14938c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f943f29e2744243b683a",
+    "url": "/static/css/main.d92d6f2b.chunk.css"
+  },
+  {
+    "revision": "e1e0a347525c422aef39",
+    "url": "/static/js/2.4307b13c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4307b13c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f943f29e2744243b683a",
+    "url": "/static/js/main.993ec4d2.chunk.js"
+  },
+  {
+    "revision": "11272b31594bf9a42557",
+    "url": "/static/js/runtime-main.af14944f.js"
+  }
+]);
